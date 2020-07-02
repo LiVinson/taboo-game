a1961bef1d736e2aae3ac8d49f363dc3c1e24098
Lisa Vinson <Lrv0828@gmail.com>
2020-07-01 20:09:18 -0400

    updated craete game comments"

--- src/components/CreateGame/CreateGame.js
+++ src/components/CreateGame/CreateGame.js
@@ -7,21 +7,22 @@ export default class CreateGame extends React.Component {
   constructor(props) {
     super(props)
 
+    //Used for Form initial values
     this.state = {
       name: "",
       gameMode: "remote",
       endGameMethod: "turns",
       turnsValue: 2,
-      timeValue:60,
+      timeValue: 60,
       skipPenalty: "none",
     }
   }
 
+  //Update to creating player id, submitting to firebase, redirecting to 'Waiting Room'
   handleSubmit(values, setSubmitting) {
-
     alert(JSON.stringify(values, null, 2))
-        //Finishes Formik submission process
-        setSubmitting(false)
+    //Finishes Formik submission process
+    setSubmitting(false)
   }
 
   handleBackClick = () => {
@@ -29,30 +30,22 @@ export default class CreateGame extends React.Component {
     this.props.history.push("/home")
   }
   render() {
-    const buttonInfo = [{text: "Back", className:"button", onClick:this.handleBackClick},{form:"createGameForm", text:"Submit", className:"button", type:"submit" }]
+    const buttonInfo = [
+      { text: "Back", className: "button", onClick: this.handleBackClick },
+      {
+        form: "createGameForm",
+        text: "Submit",
+        className: "button",
+        type: "submit",
+      },
+    ]
     return (
       <TabooCard tabooWord="New Game" type="home" buttons={buttonInfo}>
         <CreateGameForm
           initialValues={this.state}
           handleSubmit={this.handleSubmit}
-          
         />
       </TabooCard>
     )
   }
 }
-
-/*
-Change to class component that renders Form.
-Form UI:
-- Input with Label (1)
-- Set of Radio Buttons with Label (2)
-- Set of Radio Button Drop Down combos ()
-
-
-- Handle Name Input and Validation - state and Handler --> Form
-- Store selected Game Mode value - state --> Form
-- Check End Game Selection value and store - state --> Form
-- Store SKipping Penalty Value - state --> Form
-- Form able to be submitted - state --> Taboo --> ButtonGroups
-*/
