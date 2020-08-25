85f8ed00564186ab791603e85e86d22713af8337
Lisa Vinson <Lrv0828@gmail.com>
2020-08-24 22:31:39 -0400

    updated TabooCard test so snapshots pass. Removed unnecessary mount. Added structure for testing additional TabooCard components

--- src/components/shared/TabooCard/__tests__/TabooCard.test.js
+++ src/components/shared/TabooCard/__tests__/TabooCard.test.js
@@ -4,57 +4,56 @@ import renderer from 'react-test-renderer'
 import 'jest-styled-components'
 import { ThemeProvider } from 'styled-components'
 import theme from '../../../../global-design/theme'
-import { TabooCard, ButtonTabooCard, LayeredTabooCard, TabooCardTop } from '../TabooCard'
+import {
+	TabooCard,
+	ButtonTabooCard,
+	LayeredTabooCard,
+	TabooCardTop,
+	TabooWordContainer,
+	TabooList,
+	TabooBody,
+} from '../TabooCard'
 import ButtonContainer from 'components/ButtonContainer'
 
-// ------------------- Main Taboo Card--------------------
-test('TabooCard renders correctly when list is passed ', () => {
-	const props = {
-		type: 'home',
-		tabooWord: 'taboo',
-		list: ['word1', 'word2', 'word3'],
-	}
-	// requires TP due to nested components using theme prop
-	const wrapper = renderer
-		.create(
-			<ThemeProvider theme={theme}>
-				<TabooCard {...props} />
-			</ThemeProvider>
-		)
-		.toJSON()
-
-	expect(wrapper).toMatchSnapshot()
-})
-
-test('TabooCard renders children prop', () => {
-	const h1 = <h1>I am a child property</h1>
-	const props = {
-		tabooWord: 'taboo',
-		children: h1,
-	}
-
-	const wrapper = shallow(<TabooCard {...props} />)
-	expect(wrapper.contains(<h1>I am a child property</h1>)).toEqual(true)
-})
-
-test('TabooCard renders list prop if children prop is not passed', () => {
-	const props = {
+describe('TabooCard renders and functions correctly', () => {
+	const defaultProps = {
 		tabooWord: 'taboo',
 		list: ['word1', 'word2', 'word3'],
 	}
 
-	const wrapper = mount(
-		<ThemeProvider theme={theme}>
-			<TabooCard {...props} />
-		</ThemeProvider>
-	)
-	expect(wrapper.find('ul').children()).toHaveLength(props.list.length)
-})
+	// ------------------- Main Taboo Card--------------------
+	test('TabooCard renders correctly when list is passed ', () => {
+		const props = {
+			...defaultProps,
+		}
+		// requires TP due to nested components using theme prop
+		const wrapper = renderer
+			.create(
+				<ThemeProvider theme={theme}>
+					<TabooCard {...props} />
+				</ThemeProvider>
+			)
+			.toJSON()
 
+		expect(wrapper).toMatchSnapshot()
+	})
+
+	test('TabooCard renders children prop', () => {
+		const h1 = <h1>I am a child property</h1>
+		const props = {
+			...defaultProps,
+			list: null,
+			children: h1,
+		}
+
+		const wrapper = shallow(<TabooCard {...props} />)
+		expect(wrapper.contains(<h1>I am a child property</h1>)).toEqual(true)
+	})
+})
 // ------------------- Button Taboo Card--------------------
 
-test('ButtonTabooCard renders ButtonContainer', () => {
-	const props = {
+describe('ButtonTabooCard renders correctly', () => {
+	const defaultProps = {
 		tabooWord: 'taboo',
 		list: ['word1', 'word2', 'word3'],
 		buttons: [
@@ -62,138 +61,150 @@ test('ButtonTabooCard renders ButtonContainer', () => {
 			{ text: 'Next', onClick: jest.fn() },
 		],
 	}
-	const wrapper = shallow(<ButtonTabooCard {...props} />)
-	expect(wrapper.find(ButtonContainer)).toHaveLength(1)
-})
 
-test('ButtonTabooCard renders correctly when list is provided', () => {
-	const props = {
-		tabooWord: 'taboo',
-		list: ['word1', 'word2', 'word3'],
-		buttons: [
-			{ text: 'Back', onClick: jest.fn() },
-			{ text: 'Next', onClick: jest.fn() },
-		],
-	}
-	const wrapper = renderer
-		.create(
-			<ThemeProvider theme={theme}>
-				<ButtonTabooCard {...props} />
-			</ThemeProvider>
-		)
-		.toJSON()
+	test('ButtonTabooCard renders ButtonContainer', () => {
+		const props = {
+			...defaultProps,
+		}
+		const wrapper = shallow(<ButtonTabooCard {...props} />)
+		expect(wrapper.find(ButtonContainer)).toHaveLength(1)
+	})
 
-	expect(wrapper).toMatchSnapshot()
-})
+	test('ButtonTabooCard renders correctly when list is provided', () => {
+		const props = {
+			...defaultProps,
+		}
 
-test('ButtonTabooCard renders children prop if provided', () => {
-	const h1 = <h1>I am a child property</h1>
-	const props = {
-		tabooWord: 'taboo',
-		children: h1,
-		buttons: [
-			{ text: 'Back', onClick: jest.fn() },
-			{ text: 'Next', onClick: jest.fn() },
-		],
-	}
-	const wrapper = renderer
-		.create(
-			<ThemeProvider theme={theme}>
-				<ButtonTabooCard {...props} />
-			</ThemeProvider>
-		)
-		.toJSON()
+		const wrapper = renderer
+			.create(
+				<ThemeProvider theme={theme}>
+					<ButtonTabooCard {...props} />
+				</ThemeProvider>
+			)
+			.toJSON()
 
-	expect(wrapper).toMatchSnapshot()
+		expect(wrapper).toMatchSnapshot()
+	})
+
+	test('ButtonTabooCard renders children prop if provided', () => {
+		const h1 = <h1>I am a child property</h1>
+		const props = {
+			...defaultProps,
+			list: null,
+			children: h1,
+		}
+		const wrapper = renderer
+			.create(
+				<ThemeProvider theme={theme}>
+					<ButtonTabooCard {...props} />
+				</ThemeProvider>
+			)
+			.toJSON()
+
+		expect(wrapper).toMatchSnapshot()
+	})
 })
 
 // ------------------- Layered Taboo Card--------------------
 
-test('LayeredTabooCard renders correctly', () => {
-	const props = {
-		tabooWord: 'Taboo!',
-		list: ['Taboo!', 'Taboo!', 'Taboo!', 'Taboo!'],
+describe('Layered TabooCard renders correctly', () => {
+	const defaultProps = {
+		tabooWord: 'taboo',
+		list: ['Taboo!', 'Taboo!', 'Taboo!', 'Taboo!', 'Taboo!'],
 		layer: 'top',
 	}
 
-	const wrapper = renderer
-		.create(
-			<ThemeProvider theme={theme}>
-				<LayeredTabooCard {...props} />
-			</ThemeProvider>
-		)
-		.toJSON()
+	test('Top LayeredTabooCard renders correctly', () => {
+		const props = {
+			...defaultProps,
+		}
 
-	expect(wrapper).toMatchSnapshot()
+		const wrapper = renderer
+			.create(
+				<ThemeProvider theme={theme}>
+					<LayeredTabooCard {...props} />
+				</ThemeProvider>
+			)
+			.toJSON()
+
+		expect(wrapper).toMatchSnapshot()
+
+		expect(wrapper).toHaveStyleRule('position', 'absolute')
+		expect(wrapper).toHaveStyleRule('z-index', '-5')
+		expect(wrapper).toHaveStyleRule('transform', 'translate(-50%,-50%) rotate(-20deg)')
+		expect(wrapper).toHaveStyleRule('color', theme.color.accent2)
+	})
+
+	test('Middle LayeredTabooCard renders correctly', () => {
+		const props = {
+			...defaultProps,
+			layer: 'middle',
+		}
+
+		const wrapper = renderer
+			.create(
+				<ThemeProvider theme={theme}>
+					<LayeredTabooCard {...props} />
+				</ThemeProvider>
+			)
+			.toJSON()
+
+		expect(wrapper).toMatchSnapshot()
+		expect(wrapper).toHaveStyleRule('position', 'absolute')
+		expect(wrapper).toHaveStyleRule('z-index', '-10')
+		expect(wrapper).toHaveStyleRule('transform', 'translate(-50%,-50%) rotate(16deg)')
+		expect(wrapper).toHaveStyleRule('color', theme.color.accent1)
+	})
+
+	test('Bottom LayeredTabooCard has correct styles', () => {
+		const props = {
+			...defaultProps,
+			layer: 'bottom',
+		}
+
+		const wrapper = renderer
+			.create(
+				<ThemeProvider theme={theme}>
+					<LayeredTabooCard {...props} />
+				</ThemeProvider>
+			)
+			.toJSON()
+
+		expect(wrapper).toMatchSnapshot()
+		expect(wrapper).toHaveStyleRule('position', 'absolute')
+		expect(wrapper).toHaveStyleRule('z-index', '-15')
+		expect(wrapper).toHaveStyleRule('transform', 'translate(-50%,-50%) rotate(-5deg)')
+		expect(wrapper).toHaveStyleRule('color', theme.color.accent3)
+	})
+})
+// ------------------- TabooList --------------------
+test('TabooList renders correctly', () => {
+
 })
 
-test('Top LayeredTabooCard has correct styles', () => {
-	const props = {
-		tabooWord: 'Taboo!',
-		list: ['Taboo!', 'Taboo!', 'Taboo!', 'Taboo!'],
-		layer: 'top',
-	}
-
-	const wrapper = mount(
-		<ThemeProvider theme={theme}>
-			<LayeredTabooCard {...props} />
-		</ThemeProvider>
-	)
-
-	expect(wrapper).toHaveStyleRule('position', 'absolute')
-	expect(wrapper).toHaveStyleRule('z-index', '-5')
-	expect(wrapper).toHaveStyleRule('transform', 'translate(-50%,-50%) rotate(-20deg)')
-	expect(wrapper).toHaveStyleRule('color', theme.color.accent2)
-})
-
-test('Middle LayeredTabooCard has correct styles', () => {
-	const props = {
-		tabooWord: 'Taboo!',
-		list: ['Taboo!', 'Taboo!', 'Taboo!', 'Taboo!'],
-		layer: 'middle',
-	}
-
-	const wrapper = mount(
-		<ThemeProvider theme={theme}>
-			<LayeredTabooCard {...props} />
-		</ThemeProvider>
-	)
-	expect(wrapper).toHaveStyleRule('position', 'absolute')
-	expect(wrapper).toHaveStyleRule('z-index', '-10')
-	expect(wrapper).toHaveStyleRule('transform', 'translate(-50%,-50%) rotate(16deg)')
-	expect(wrapper).toHaveStyleRule('color', theme.color.accent1)
-})
-
-test('Bottom LayeredTabooCard has correct styles', () => {
-	const props = {
-		tabooWord: 'Taboo!',
-		list: ['Taboo!', 'Taboo!', 'Taboo!', 'Taboo!'],
-		layer: 'bottom',
-	}
-
-	const wrapper = mount(
-		<ThemeProvider theme={theme}>
-			<LayeredTabooCard {...props} />
-		</ThemeProvider>
-	)
-	expect(wrapper).toHaveStyleRule('position', 'absolute')
-	expect(wrapper).toHaveStyleRule('z-index', '-15')
-	expect(wrapper).toHaveStyleRule('transform', 'translate(-50%,-50%) rotate(-5deg)')
-	expect(wrapper).toHaveStyleRule('color', theme.color.accent3)
+// ------------------- TabooListItem --------------------
+test('TabooListItem renders correctly', () => {
+	
 })
 
 // ------------------- Taboo Card Top --------------------
 
 test('TabooCardTop renders correctly', () => {
-  const props = {
-		tabooWord: 'Taboo!'
+	const props = {
+		tabooWord: 'Taboo!',
 	}
 	const wrapper = renderer
 		.create(
 			<ThemeProvider theme={theme}>
-				<TabooCardTop {...props}/>
+				<TabooCardTop {...props} />
 			</ThemeProvider>
 		)
 		.toJSON()
 	expect(wrapper).toMatchSnapshot()
+})
+
+
+// ------------------- FilteredTabooList --------------------
+test('FilteredTabooList renders correctly', () => {
+	
 })
